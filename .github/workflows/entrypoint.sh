4bc55325e1dfeb3e6e0d955ec8eca15c09446c33
sridish123 <82566673+sridish123@users.noreply.github.com>
2021-05-03 20:43:02 +0530

    Update entrypoint.sh

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Install mecab, then build wheels
 set -e
+manylinux_version=1
+plat=x86_64
 
 # install MeCab
 # TODO specify the commit used here
 git clone --depth=1 git://github.com/taku910/mecab.git
 cd mecab/mecab
 if [ `uname -m` == 'aarch64' ]; then
+  manylinux_version=2014
+  plat=aarch64
   yum -y update && yum install -y wget
   wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' -O config.guess
   wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD' -O config.sub
@@ -28,16 +32,8 @@ done
 
 # fix the wheels (bundles libs)
 for wheel in /github/workspace/wheels/*.whl; do
-  if [ `uname -m` == 'aarch64' ]; then
-    auditwheel repair "$wheel" --plat manylinux2014_aarch64 -w /github/workspace/manylinux2014-wheels
-  else
-    auditwheel repair "$wheel" --plat manylinux1_x86_64 -w /github/workspace/manylinux1-wheels
-  fi
+    auditwheel repair "$wheel" --plat manylinux${manylinux_version}_${plat} -w /github/workspace/manylinux${manylinux_version}-wheels
 done
 
 echo "Built wheels:"
-if [ `uname -m` == 'aarch64' ]; then
-  ls /github/workspace/manylinux2014-wheels
-else
-  ls /github/workspace/manylinux1-wheels
-fi
+ls /github/workspace/manylinux${manylinux_version}-wheels
